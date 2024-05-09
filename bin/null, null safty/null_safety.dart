265750cd3ac7848ae4c8d56a4683fef3c8277fbb
MuhammadFerrySofianshah <fsofian27@gmail.com>
2024-05-09 20:55:50 +0700

    null safety

--- bin/null, null safty/null_safety.dart
+++ bin/null, null safty/null_safety.dart
@@ -1,6 +1,29 @@
 void main(List<String> args) {
-  var age;
-  double ageDouble = age.toDouble();
+  // konversi nilai null int ke double
+  // contoh 1
+  int? age = 10;
+  print(age.toDouble());
 
-  print(ageDouble);
+  // konversi dari nulllabe ke not nullable
+  // contoh 2
+  String name = 'ferry'; // non nullable
+  String? nullLableName = name; // nulllable
+  int? nullableNumber;
+  //  harus mengececk dengan null lable check
+  if (nullableNumber != null) {
+    int number = nullableNumber;
+  }
+
+  // default value
+  // jika nilainya null makanya akan menampilkan defaultnya
+  String? nama;
+  // defaultnya disini
+  String tes = nama ?? 'nilai default';
+  print(tes);
+
+  int? angka ; // nullbale
+
+  int mAngka = angka!;
+
+  print(mAngka) ;
 }
